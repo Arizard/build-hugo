d816f6ee23b3b8e33d5533056010dceb1d55abb4
Arie Oldman <arie.oldman@vhs7.tv>
2026-05-16 11:32:17 +1000

    installs npm packages inside the builder

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-INSTALL_NODE=$1
+INSTALL_NPM_PACKAGES=$1
 
-if [ "$INSTALL_NODE" = "true" ]; then
-  echo "installing node"
-  apt-get update -y && apt-get install node -y
+if [ "$INSTALL_NPM_PACKAGES" = "true" ]; then
+  echo "installing node and npm"
+  apt-get update -y && apt-get install node npm -y
+  echo "installing packages"
+  npm i
 fi
 
 shift
